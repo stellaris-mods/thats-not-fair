4cf8ca0b07e3e53ee4340439eba0ae4e124c343b
Folk <vhaarr@gmail.com>
2017-12-07 17:36:39 +0100

    Bump to 1.9.x.

--- modinfo.lua
+++ modinfo.lua
@@ -4,8 +4,9 @@ return {
 	name = "That's Not Fair!",
 	tags = { "Balance", "Gameplay", "Economy", "Galaxy Generation" },
 	picture = "thumb.png",
-	supported_version = "1.8.*",
+	supported_version = "1.9.*",
 	remote_file_id = 799159083,
+	originalUploader = "folk",
 	readme = "readme.md",
 	steambb = "steam.bbcode",
 }
